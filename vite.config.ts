931d056df0818953afa28228f1ce3b7c4a4b0425
Yuukub <sarnyou@gmail.com>
2026-02-04 23:24:12 +0700

    chore: optimize vite build with manual chunks and increased size limit

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,16 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
